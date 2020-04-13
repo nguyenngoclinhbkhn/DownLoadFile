fa8d54222cc5a3564203d8de64ece0fadb218d18
nguyenlinh <nguyenngoclinhbkhn@gmail.com>
2020-04-13 08:05:50 +0700

    Add option down ver 3

--- app/src/main/java/com/example/downloadfile/MainActivity.kt
+++ app/src/main/java/com/example/downloadfile/MainActivity.kt
@@ -15,6 +15,8 @@ import com.example.downloadfile.client.Client
 import com.example.downloadfile.model.Photo
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.Single
+import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -39,6 +41,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var progressDialog: ProgressDialog? = null
     private val listPhoto = ArrayList<Photo>()
     private var listFake = ArrayList<Photo>()
+    private val numberSize = 1000
     private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +63,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         btnDownVer1.setOnClickListener(this)
         btnDownVer2.setOnClickListener(this)
+        btnDownVer3.setOnClickListener(this)
     }
 
 
@@ -115,19 +119,60 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                         cancelDialog()
                     }
 
-                    override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
+                    override fun onResponse(
+                        call: Call<List<Photo>>,
+                        response: Response<List<Photo>>
+                    ) {
                         if (response != null) {
-                           dowVer2()
+                            dowVer2()
                         }
                     }
                 })
 
 
             }
+            R.id.btnDownVer3 -> {
+//                val it = getFake()
+//                val distance = it.size / 3
+//                val listTop: ArrayList<List<Photo>> = arrayListOf()
+//                var start = 0
+//                var end = distance;
+//                var count = 0;
+//                listTop.clear()
+//                while (true) {
+//                    if (end >= it.size) break
+//                    val list = arrayListOf<Photo>()
+//                    for (i in start until end) {
+//                        list.add(it[i])
+//                    }
+//                    listTop.add(list)
+//                    start += distance
+//                    end += distance
+//
+//                    count++
+//                }
+//                val list = arrayListOf<Photo>()
+//                for (i in (start) until it.size) {
+//                    list.add(it[i])
+//                }
+//                listTop.add(list)
+//                Log.e("TAG", "count $count")
+//                Log.e("TAG", "size listtop ${listTop.size}")
+//
+//                for (test in 0 until listTop.size) {
+//                    Log.e("TAG", "Size ${listTop[test].size}  ${listTop[test]}")
+////                    createObservableVer3(test).subscribe(getObserverVer3())
+//                }
+//                for (test in 0 until listTop.size) {
+//                    Log.e("TAG", "size ver 2 ${listTop[test].size}")
+//                }
+
+                downVer3()
+            }
         }
     }
 
-    private fun dowVer1(){
+    private fun dowVer1() {
         val intThreadCount = Runtime.getRuntime().availableProcessors() - 1
         val executorService = Executors.newFixedThreadPool(intThreadCount)
         val schedulers = Schedulers.from(executorService)
@@ -135,7 +180,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         singlePhoto.subscribeOn(Schedulers.io())
             .flatMap {
                 Observable.create<Photo> { emitter ->
-                    listFake.forEach { it -> // fake list lấy từ json vì url ảnh của json ko dow ảnh được
+                    listFake.forEach { it ->
+                        // fake list lấy từ json vì url ảnh của json ko dow ảnh được
                         val bitmap = getBitmapFromURL(it.url)
                         savePhoto(bitmap)
                         emitter.onNext(it)
@@ -150,20 +196,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     Log.e("OnComplete", "OnComplete ${listPhoto.size}")
                     cancelDialog()
                 }
+
                 override fun onSubscribe(d: Disposable) {
                 }
+
                 override fun onNext(t: Photo) {
                     Log.e("TAG", "onNext $t")
                     listPhoto.add(t)
-                    progressDownload.progress = listPhoto.size * 100 / 4999
-                    txtPercent.text = " ${listPhoto.size * 100 / 4999}  %"
+                    progressDownload.progress = listPhoto.size * 100 / numberSize
+                    txtPercent.text = " ${listPhoto.size * 100 / numberSize}  %"
                 }
+
                 override fun onError(e: Throwable) {
                 }
             })
     }
 
-    private fun dowVer2(){
+    private fun dowVer2() {
         var count = 0
         val executors =
             Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1)
@@ -172,8 +221,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 savePhoto(getBitmapFromURL(it.url))
                 count++
                 val message = Message();
-                    message.arg1 = count;
-                    handler.sendMessage(message);
+                message.arg1 = count;
+                handler.sendMessage(message);
 
             })
             executors.execute(thread)
@@ -184,6 +233,85 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
         cancelDialog()
 
+    }
+
+    private fun downVer3() {
+        val count = Runtime.getRuntime().availableProcessors() - 1;
+        val single = Client.createAPI().getListPhoto();
+        progressDownload.max = 101
+        single.subscribeOn(Schedulers.io())
+            .map { it ->
+                getFake()
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ it ->
+                val distance = it.size / count
+                val listTop: ArrayList<List<Photo>> = arrayListOf()
+                var start = 0
+                var end = distance;
+                listTop.clear()
+                while (true) {
+                    if (end >= it.size) break
+                    val list = arrayListOf<Photo>()
+                    for (i in start until end) {
+                        list.add(it[i])
+                    }
+                    listTop.add(list)
+                    start += distance
+                    end += distance
+                }
+                val list = arrayListOf<Photo>()
+                for (i in (start) until it.size) {
+                    list.add(it[i])
+                }
+                listTop.add(list)
+               for (test in 0 until listTop.size) {
+                   Log.e("TAG", "Size ${listTop[test].size}  ${listTop[test]}")
+                    createObservableVer3(listTop[test]).subscribe(getObserverVer3())
+               }
+            },
+                { error ->
+                    Log.e("TAG", "Error ${error.toString()}")
+                })
+
+    }
+
+    private fun createObservableVer3(list: List<Photo>): Observable<Photo> {
+        return object : Observable<Photo>() {
+            override fun subscribeActual(observer: Observer<in Photo>?) {
+                for (test in list) {
+                    savePhoto(getBitmapFromURL(test.url))
+                    observer?.onNext(test)
+                }
+                observer?.onComplete()
+            }
+
+        }.subscribeOn(Schedulers.from(Executors.newFixedThreadPool(1)))
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    private var countVer3 = 0;
+    private fun getObserverVer3(): Observer<Photo> {
+        return object : Observer<Photo> {
+            override fun onComplete() {
+                Log.e("TAG", "complete")
+
+            }
+
+            override fun onSubscribe(d: Disposable) {
+            }
+
+            override fun onNext(t: Photo) {
+                countVer3++;
+                progressDownload.progress = countVer3 * 100 / numberSize
+                txtPercent.text = " ${countVer3 * 100 / numberSize}  %"
+                Log.e("TAG", "countVer3 $countVer3")
+            }
+
+            override fun onError(e: Throwable) {
+            }
+
+        }
     }
 
     private fun showDialog() {
@@ -198,7 +326,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             progressDialog?.cancel()
         }
     }
-
 
 
     fun getBitmapFromURL(src: String?): Bitmap? {
@@ -221,10 +348,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun createFake(){
-        for (i in 0..4999){
-            listFake.add(Photo(1,1, "sdf", "http://placehold.jp/150x150.png", "sdf"))
+    private fun createFake() {
+        for (i in 0..numberSize) {
+            listFake.add(Photo(1, 1, "sdf", "http://placehold.jp/150x150.png", "sdf"))
         }
+    }
+
+    private fun getFake(): List<Photo> {
+        var listFake = ArrayList<Photo>()
+        for (i in 0..numberSize) {
+            listFake.add(Photo(1, 1, "sdf", "http://placehold.jp/150x150.png", "sdf"))
+        }
+
+        return listFake
     }
 
 
